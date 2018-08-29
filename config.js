fb9bd6f8a1806a937049310a88fbdd64a86173a6
Nathan <nathancleon@gmail.com>
2018-08-29 13:30:04 -0400

    added process env port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DB_URL = 'mongodb://user:pass123@ds161262.mlab.com:61262/healthy-plantdb';
-exports.PORT = 8080;
+exports.PORT = process.env.PORT || 8080;
 exports.TEST_PORT = 3000;
 exports.DB_TEST_URL = 'mongodb://user:pass123@ds163680.mlab.com:63680/healthy-plant-testdb';
 exports.PASSPORT_SECRET = 'iloveplantsmorethananythingever';
